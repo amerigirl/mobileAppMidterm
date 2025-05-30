839aca52f397ad5224d132464562c8180fb55d01
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-29 20:25:44 -0700

    added intent for start button :)

--- app/src/main/java/com/example/mobileappmidterm/MainActivity.kt
+++ app/src/main/java/com/example/mobileappmidterm/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobileappmidterm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //button
+        val startButton = findViewById<Button>(R.id.button)
+
+        startButton.setOnClickListener {
+            val start = Intent(this, throwDie2::class.java)
+            startActivity(start)
         }
     }
 }
