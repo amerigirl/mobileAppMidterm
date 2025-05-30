e55fbf53b3cbad57cf95cc2105a4ed34bc123018
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-29 21:09:46 -0700

    added recyclerview logic to dependencies; updating adapter with viewHolder class

---
+++ app/src/main/java/com/example/mobileappmidterm/myAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.mobileappmidterm
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class myAdapter (private val list: List<dieRoll>): RecyclerView.Adapter<myAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: myAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+
+    class ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
