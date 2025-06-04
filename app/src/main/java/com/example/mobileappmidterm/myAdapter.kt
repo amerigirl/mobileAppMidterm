b836e6a0655023f5d02186c21e4b966d9a3750c1
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-04 12:19:43 -0700

    fixed viewHolder Error

--- app/src/main/java/com/example/mobileappmidterm/myAdapter.kt
+++ app/src/main/java/com/example/mobileappmidterm/myAdapter.kt
@@ -1,27 +1,20 @@
 package com.example.mobileappmidterm
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class myAdapter (private val list: List<dieRoll>): RecyclerView.Adapter<myAdapter.ViewHolder>() {
+class myAdapter(private val list: List<dieRoll>) : RecyclerView.Adapter<myAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: myAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-
-    class ViewHolder {
-
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-
+    override fun getItemCount(): Int = list.size
 }
